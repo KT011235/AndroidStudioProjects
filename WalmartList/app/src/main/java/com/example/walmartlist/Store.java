9d8f1d78363759936d8f1c23548a118f3686d92a
Keito <katkeono2002@gmail.com>
2025-03-11 16:33:08 -0400

    Done editing walmart list.

--- WalmartList/app/src/main/java/com/example/walmartlist/Store.java
+++ WalmartList/app/src/main/java/com/example/walmartlist/Store.java
@@ -12,7 +12,23 @@ public class Store {
     public Store(JSONObject object) throws JSONException {
         this.name = object.getString("name");
         this.address = object.getString("street_address");
+        this.phone = object.getString("phone_number_1");
         this.city = object.getString("city");
-        this.phone = object.getString("phone");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getCity() {
+        return city;
     }
 }
