3cfcb000d1235dcd75ef5c5e90e9a26413f3a000
Keito <katkeono2002@gmail.com>
2025-04-24 18:17:21 -0400

    Final project done.

---
+++ CustomerList/app/src/main/java/com/example/customerlist/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.customerlist;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    protected ListView list;
+    protected String url = "http://10.2.105.73:5000/all";
+    private Gson gson;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        gson = gsonBuilder.create();
+
+        list = (ListView) findViewById(R.id.listView);
+        List<Customer> customers = new ArrayList<Customer>();
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        queue.start();
+
+        JsonArrayRequest jsonArrayRequest =
+                new JsonArrayRequest(Request.Method.GET,
+                        url, null, new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        for (int i = 0; i < response.length(); i++) {
+                            try {
+                                Customer cus = gson.fromJson(response.getJSONObject(i).toString(), Customer.class);
+                                customers.add(cus);
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        CustomerAdapter adapter = new CustomerAdapter(list.getContext(), customers);
+                        //list.setOnItemClickListener(adapter);
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("JSONArray Error", "Error:" + error);
+                    }
+                });
+
+        queue.add(jsonArrayRequest);
+    }
+}
